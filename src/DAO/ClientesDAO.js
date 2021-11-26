b67d88c8f79d524c35d2c936b95e4ee988d7a70a
LuthGom <lucianomendes23@yahoo.com>
2021-11-26 17:42:05 -0300

    Faltando apenas o Update e Delete do CRUD

--- src/DAO/ClientesDAO.js
+++ src/DAO/ClientesDAO.js
@@ -1,10 +1,30 @@
 class ClientesDAO {
-    constructor(bd) {
-        this.bd = bd
+    constructor(db) {
+        this.db = db
+    }
+    insereCliente(novoCliente) {
+        console.log("DAOINFERNO");
+        return new Promise((resolve, reject) => {
+            this.db.run(`INSERT INTO CLIENTES (CPF, NOME, TELEFONE, CEP, ENDERECO, CIDADE, UF, EMAIL, SENHA) VALUES (?,?,?,?,?,?,?,?,?);`,
+                [novoCliente.cpf, novoCliente.nome, novoCliente.telefone, novoCliente.cep, novoCliente.endereco, novoCliente.cidade, novoCliente.uf, novoCliente.email, novoCliente.senha],
+                (error) => {
+                    if (error) {
+                        reject({
+                            "mensagem": error.message,
+                            "erro": true
+                        })
+                    } else {
+                        resolve({
+                            "requisicao": novoCliente,
+                            "erro": false
+                        })
+                    }
+                })
+        })
     }
     pegaTodosClientes() {
         return new Promise((resolve, reject) => {
-            this.bd.all('SELECT * FROM CLIENTES', (error, rows) => {
+            this.db.all('SELECT * FROM CLIENTES;', (error, rows) => {
 
                 if (error) {
                     reject({
@@ -21,25 +41,49 @@ class ClientesDAO {
             })
         })
     }
-    insereCliente(novoCliente) {
-        console.log("DAOINFERNO");
+
+    retornaClientesDesejados(id) {
+        console.log(id);
+        const SELECT_BY_ID = `SELECT * FROM CLIENTES WHERE ID = ?`
         return new Promise((resolve, reject) => {
-            this.bd.run(`INSERT INTO CLIENTES (CPF, NOME, TELEFONE, CEP, ENDERECO, CIDADE, UF, EMAIL, SENHA) VALUES (?,?,?,?,?,?,?,?,?);`,
-                [novoCliente.cpf, novoCliente.nome, novoCliente.telefone, novoCliente.cep, novoCliente.endereco, novoCliente.cidade, novoCliente.uf, novoCliente.email, novoCliente.senha],
-                (error) => {
-                    if (error) {
-                        reject({
-                            "mensagem": error.message,
-                            "erro": true
-                        })
-                    } else {
-                        resolve({
-                            "requisicao": novoCliente,
-                            "erro": false
-                        })
-                    }
-                })
+            this.db.all(SELECT_BY_ID,id, (error, rows) => {
+                if(error) {
+                    reject ({
+                        "mensagem": error.message,
+                        "erro": true
+                    })
+                } else {
+                    resolve({
+                        "requisicao": rows,
+                        "erro": false
+                    })
+                }
+            })
         })
+    }
+    atualizaCliente(id, cliente) {
+        try {
+            const UPDATE = `
+            UPDATE CLIENTES
+            SET CPF = ?, NOME = ?, TELEFONE = ?, CEP = ?, ENDERECO = ?, CIDADE = ?, UF = ?, EMAIL = ?, SENHA = ?`
+            return new Promise ((resolve, reject)=> {
+                this.db.run(UPDATE, [cliente.cpf, cliente.nome, cliente.telefone, cliente.cep, cliente.endereco, cliente.cidade, cliente.uf, cliente.email, cliente.senha],id,
+                    (error)=>{
+                        if(error) {
+                            reject(error)
+                        } else {
+                            resolve({
+                                "mensagem": `Cliente de id ${id} atualizado com sucesso.`,
+                                "usuario": cliente,
+                                "erro": false
+                            })
+                        }
+                    })
+            })
+        } catch (error){
+            console.log(Error);
+            throw new Error(error.message)
+        }
     }
 }
 module.exports = ClientesDAO
--- src/controllers/clientes-controllers.js
+++ src/controllers/clientes-controllers.js
@@ -2,33 +2,96 @@ const Cliente = require('../models/Clientes')
 const ClientesDAO = require('../DAO/ClientesDAO')
 const clientes = (app, bd) => {
   const novoClienteDAO = new ClientesDAO(bd)
-  app.get('/clientes', (req, res) => {
-    novoClienteDAO.pegaTodosClientes()
-      .then((resposta) => {
-        res.json(resposta)
-      })
-      .catch((erro) => {
-        res.json(erro)
-      })
-  })
-  app.post('/clientes', (req, res) => {
+  app.post('/clientes', async (req, res) => {
 
     try {
       const body = req.body
       const novoCliente = new Cliente(body.cpf, body.nome, body.telefone, body.cep, body.endereco, body.cidade, body.uf, body.email, body.senha)
-      novoClienteDAO.insereCliente(novoCliente)
-        .then((resposta) => {
-          console.log('oka');
-          res.send(resposta)
+      const resposta = await novoClienteDAO.insereCliente(novoCliente)
+      res.json(resposta)
+    } catch (error) {
+      res.json(error)
+    }
+  })
+  app.get('/clientes', async (req, res) => {
+    try {
+      const resposta = await novoClienteDAO.pegaTodosClientes()
+
+      res.json(resposta)
+    } catch {
+      res.json(error)
+    }
+  })
+
+  app.get('/clientes/:id', async (req, res) => {
+    const id = req.params.id
+    console.log(id);
+    try {
+      const resposta = await novoClienteDAO.retornaClientesDesejados(id)
+      console.log('565s65s6a5s6a5');
+      res.json(resposta)
+    } catch (error){
+      console.log('fuck');
+      res.json(error)
+
+    }
+
+  })
+
+  app.put('/clientes/:id', async(req, res) => {
+    const id = req.params.id
+    const body = req.body
+
+    try {
+      const respostaGet = await novoClienteDAO.retornaClientesDesejados(id,body)
+      const clienteAntigo = respostaGet.requisicao[0]
+
+
+      if (clienteAntigo) {
+        const clienteAtualizado = new Cliente(
+          body.cpf || clienteAntigo.CPF,
+          body.nome || clienteAntigo.NOME,
+          body.telefone || clienteAntigo.TELEFONE,
+          body.cep || clienteAntigo.CEP,
+          body.endereco || clienteAntigo.ENDERECO,
+          body.cidade || clienteAntigo.CIDADE,
+          body.uf || clienteAntigo.UF,
+          body.email || clienteAntigo.EMAIL,
+          body.senha || clienteAntigo.SENHA,
+        )
+        const resposta = await novoClienteDAO.atualizaCliente(id, clienteAtualizado)
+        res.json(resposta)
+      } else {
+        res.json({
+          "mensagem": `Cliente com cpf "${id} não encontrado`,
+          "error": true
         })
-        .catch((erro) => {
-          res.send(erro)
-        })
+      }
     } catch (error) {
       console.log(error);
       res.json({
-        "m": error.message,
-        "erro": true
+        "mensagem": error.message,
+        "error": true
+      })
+    }
+  })
+
+  app.delete('clientes/:cpf', (req, res) => {
+    const cpf = req.params.cpf
+
+
+    const indexCliente = bd.clientes.findIndex((clientes => clientes.cpf === cpf))
+
+    if (indexCliente > -1) {
+      const clienteDeletado = bd.clientes.splice(indexCliente, 1)
+      res.json({
+        "deletado": clienteDeletado,
+        "error": false
+      })
+    } else {
+      res.json({
+        "mensagem": `Cliente com cpf "${cpf}" inexistente.`,
+        "error": true
       })
     }
   })
