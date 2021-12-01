9c5ac02b3abd84d1686c6890aee4754fe9b8da41
LuthGom <lucianomendes23@yahoo.com>
2021-12-01 01:38:01 -0300

    Implementando o repositório ao Heroku

--- src/DAO/ClientesDAO.js
+++ src/DAO/ClientesDAO.js
@@ -3,7 +3,6 @@ class ClientesDAO {
         this.db = db
     }
     insereCliente(novoCliente) {
-        console.log("DAOINFERNO");
         return new Promise((resolve, reject) => {
             this.db.run(`INSERT INTO CLIENTES (CPF, NOME, TELEFONE, CEP, ENDERECO, CIDADE, UF, EMAIL, SENHA) VALUES (?,?,?,?,?,?,?,?,?);`,
                 [novoCliente.cpf, novoCliente.nome, novoCliente.telefone, novoCliente.cep, novoCliente.endereco, novoCliente.cidade, novoCliente.uf, novoCliente.email, novoCliente.senha],
--- src/infra/sqlite-db.js
+++ src/infra/sqlite-db.js
@@ -2,10 +2,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 const caminhoArq = path.resolve(__dirname,'database.db')
-// Esse comando verifica se existe um bd no caminha especificado
-// Se nao existir ele cria um com o nome dado
-// Por isso a importancia de ser o mesmo caminho que o indicado no arquivo
-// que cria e popula as tabelas
+
 const bd = new sqlite3.Database(caminhoArq);
 
 
