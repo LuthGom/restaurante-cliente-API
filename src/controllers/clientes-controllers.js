4aed4517b4aee14e49631802d6289130f8d96e9e
LuthGom <lucianomendes23@yahoo.com>
2022-03-07 11:10:08 -0300

    removendo console.log

--- src/controllers/clientes-controllers.js
+++ src/controllers/clientes-controllers.js
@@ -69,7 +69,6 @@ class ClientesController {
         },
       });
     } catch (error) {
-      // console.log(error);
       res.status(500).json({
         message: error.message,
         error: true,
