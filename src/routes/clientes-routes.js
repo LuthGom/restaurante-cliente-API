fc530f23813bb85520055b21cc111aa93f422799
LuthGom <lucianomendes23@yahoo.com>
2022-05-07 23:21:34 -0300

    Routes: add autenticação por token, com método bearer, para a rota de update com o método patch

--- src/routes/clientes-routes.js
+++ src/routes/clientes-routes.js
@@ -1,14 +1,13 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
-const clientesController = require('../controllers/clientes-controllers');
-const { local, bearer } = require('../middlewares/middlewaresAutenticacao')
+const clientesController = require("../controllers/clientes-controllers");
+const { local, bearer } = require("../middlewares/middlewaresAutenticacao");
 
-module.exports =
-    router
-        .post('/cliente', clientesController.cadastrarCliente)
-        .post('/cliente/login', local, clientesController.login)
-        .get('/cliente/logout', bearer, clientesController.logout)
-        .get('/clientes', clientesController.listarTodosOsClientes)
-        .get('/cliente/:id', clientesController.listarClientePorId)
-        .delete('/cliente/:cpf', bearer, clientesController.deletarCliente)
-        .patch('/cliente/:cpf', clientesController.atualizarCliente)
+module.exports = router
+  .post("/cliente", clientesController.cadastrarCliente)
+  .post("/cliente/login", local, clientesController.login)
+  .get("/cliente/logout", bearer, clientesController.logout)
+  .get("/clientes", clientesController.listarTodosOsClientes)
+  .get("/cliente/:id", clientesController.listarClientePorId)
+  .delete("/cliente/:cpf", bearer, clientesController.deletarCliente)
+  .patch("/cliente/:cpf", bearer, clientesController.atualizarCliente);
