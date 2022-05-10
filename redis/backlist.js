df8e77b6b10ed7bfa3e3ab4d8f9545afd819fe1c
LuthGom <lucianomendes23@yahoo.com>
2022-05-10 16:32:58 -0300

    redis conexao: passando as strings de url e passoword de conexao cloud para uma variavel de ambiente para maior segurança.

--- redis/backlist.js
+++ redis/backlist.js
@@ -2,8 +2,8 @@ const { createClient } = require("redis");
 
 // let blacklist;
 const blacklist = createClient({
-  url: "redis://redis-15941.c258.us-east-1-4.ec2.cloud.redislabs.com:15941",
-  password: "cOH8sucMdVQLWBQXOBq6nx9BwsBp6xUL"
+  url: process.env.REDIS_URL,
+  password: process.env.REDIS_PASSWORD
 });
 blacklist.on("connect", () => console.log("Connected to Redis!"));
 blacklist.on("error", (err) => console.log("Redis Client Error", err));
