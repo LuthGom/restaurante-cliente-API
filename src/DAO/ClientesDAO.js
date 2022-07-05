700c21cbe04bc268ca369b82efa4f16ed66ac08b
LuthGom <lucianomendes23@yahoo.com>
2022-07-04 23:21:38 -0300

    DAO:<ClientesDAO.js>:  alterando as queries com a sintaxe atualizada para o bando de dados PostGreSQL

--- src/DAO/ClientesDAO.js
+++ src/DAO/ClientesDAO.js
@@ -1,129 +1,144 @@
-const db = require('../infra/sqlite-db')
+const db = require("../infra/dbPG");
 class ClientesDAO {
-
-    static cadastrarCliente(novoCliente) {
-        return new Promise((resolve, reject) => {
-            db.all(`INSERT INTO CLIENTES (cpf, nome, telefone, cep, endereco, cidade, uf, email, senhaHash) VALUES (?,?,?,?,?,?,?,?,?);`,
-                [...Object.values(novoCliente)],
-                (error, rows) => {
-                    if (error) {
-                        reject({
-                            "mensagem": error.message,
-                            "erro": true
-                        })
-                    }
-                    return resolve({ "return": rows });
-
-                })
-        })
-    }
-    static listarTodosOsClientes() {
-        return new Promise((resolve, reject) => {
-            db.all('SELECT * FROM CLIENTES;', (error, rows) => {
-
-                if (error) {
-                    reject({
-                        "mensagem": error.message,
-                        "error": true
-                    })
-                } else {
-                    resolve({
-                        "clientes": rows,
-                        "error": false
-                    })
-                }
-            })
-        })
-    }
-
-    static listarClientePorId(id) {
-        const SELECT_BY_ID = `SELECT * FROM CLIENTES WHERE ID = ?`
-        return new Promise((resolve, reject) => {
-            db.all(SELECT_BY_ID, id, (error, rows) => {
-                if (error) {
-                    reject({
-                        "mensagem": error.message,
-                        "erro": true
-                    })
-                } else {
-                    resolve({
-                        "requisicao": rows,
-                        "erro": false
-                    })
-                }
-            })
-        })
-    }
-    static deletarCliente(cpf) {
-        return new Promise((resolve, reject) => {
-            const deletar = `DELETE FROM CLIENTES WHERE CPF = ?`
-            db.run(deletar, cpf, (error) => {
-                if (error) {
-                    reject({
-                        "mensagem": error.message,
-                        "error": true
-                    })
-                } else {
-                    resolve({
-                        "mensagem": `Cliente de CPF ${cpf} excluído com sucesso!`,
-                        "erro": false
-                    })
-                }
-            })
-        })
-    }
-    static buscaPorEmail(email) {
-        const SELECT_BY_EMAIL = `SELECT * FROM CLIENTES WHERE EMAIL = ?`
-        return new Promise((resolve, reject) => {
-            db.get(SELECT_BY_EMAIL, email, (error, rows) => {
-                if (error) {
-                    reject({
-                        "mensagem": error.message,
-                        "erro": true
-                    })
-                } else {
-                    resolve(rows)
-                }
-            })
-        })
-    }
-    static buscaPorCpf(cpf) {
-        const SELECT_BY_EMAIL = `SELECT * FROM CLIENTES WHERE CPF = ?`
-        return new Promise((resolve, reject) => {
-            db.get(SELECT_BY_EMAIL, cpf, (error, rows) => {
-                if (error) {
-                    reject({
-                        "mensagem": error.message,
-                        "erro": true
-                    })
-                }
-                resolve(rows)
-
-            })
-        })
-    }
-    static atualizarCliente(cpf, cliente) {
-
-        return new Promise((resolve, reject) => {
-            const UPDATE = `
-                UPDATE CLIENTES
-                SET cpf = ?, nome = ?, telefone = ?, cep = ?, endereco = ?, cidade = ?, uf = ?, email = ?, senhaHash = ? WHERE cpf = ?`
-            const array = [...Object.values(cliente), cpf]
-            db.run(UPDATE,
-                array,
-                (error) => {
-                    if (error) {
-                        reject(error)
-                    } else {
-                        resolve({
-                            "mensagem": `Cliente de cpf ${cpf} atualizado com sucesso.`,
-                            "cliente": cliente,
-                            "erro": false
-                        })
-                    }
-                })
-        })
-
-    }
+  static cadastrarCliente(novoCliente) {
+    return new Promise((resolve, reject) => {
+      db.query(
+        `INSERT INTO "CLIENTES" ("cpf", "nome", "telefone", "cep", "endereco", "cidade", "uf", "email", "senhaHash") VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9);`,
+        [
+          novoCliente.cpf,
+          novoCliente.nome,
+          novoCliente.telefone,
+          novoCliente.cep,
+          novoCliente.endereco,
+          novoCliente.cidade,
+          novoCliente.uf,
+          novoCliente.email,
+          novoCliente.senhaHash,
+        ],
+        (err, res) => {
+          if (err) {
+            reject({
+              mensagem: err.message,
+              erro: true,
+            });
+          }
+          return resolve(res.rows[0]);
+        }
+      );
+    });
+  }
+  static listarTodosOsClientes() {
+    return new Promise((resolve, reject) => {
+      db.query(`SELECT * FROM "CLIENTES"`, (err, res) => {
+        if (err) {
+          reject({
+            mensagem: err.message,
+            erro: true,
+          });
+        } else {
+          resolve({
+            clientes: res.rows,
+            erro: false,
+          });
+        }
+      });
+    });
+  }
+  static listarClientePorId(id) {
+    const SELECT_BY_ID = `SELECT * FROM "CLIENTES" WHERE ID = ?`;
+    return new Promise((resolve, reject) => {
+      db.query(SELECT_BY_ID, id, (err, res) => {
+        if (err) {
+          reject({
+            mensagem: err.message,
+            erro: true,
+          });
+        } else {
+          resolve({
+            requisicao: res.rows[0],
+            erro: false,
+          });
+        }
+      });
+    });
+  }
+  static deletarCliente(cpf) {
+    return new Promise((resolve, reject) => {
+      const deletar = `DELETE FROM "CLIENTES" WHERE "cpf" = $1`;
+      db.query(deletar, [cpf], (err) => {
+        if (err) {
+          reject({
+            mensagem: err.message,
+            erro: true,
+          });
+        } else {
+          resolve({
+            mensagem: `Cliente de CPF ${cpf} excluído com sucesso!`,
+            erro: false,
+          });
+        }
+      });
+    });
+  }
+  static buscaPorEmail(email) {
+    const SELECT_BY_EMAIL = `SELECT * FROM "CLIENTES" WHERE "email" = $1`;
+    return new Promise((resolve, reject) => {
+      db.query(SELECT_BY_EMAIL, [email], (err, res) => {
+        if (err) {
+          reject({
+            mensagem: err.message,
+            erro: true,
+          });
+        }
+        resolve(res.rows[0]);
+      });
+    });
+  }
+  static buscaPorCpf(cpf) {
+    const SELECT_BY_EMAIL = `SELECT * FROM "CLIENTES" WHERE "cpf" = $1`;
+    return new Promise((resolve, reject) => {
+      db.query(SELECT_BY_EMAIL, [cpf], (err, res) => {
+        if (err) {
+          reject({
+            mensagem: err.message,
+            erro: true,
+          });
+        }
+        resolve(res.rows[0]);
+      });
+    });
+  }
+  static atualizarCliente(cpf, cliente) {
+    return new Promise((resolve, reject) => {
+      const UPDATE = `
+                UPDATE "CLIENTES"
+                SET "cpf" = $1, "nome" = $2, "telefone" = $3, "cep" = $4, "endereco" = $5, "cidade" = $6, "uf" = $7, "email" = $8, "senhaHash" = $9 WHERE "cpf" = $1`;
+      db.query(
+        UPDATE,
+        [
+          cliente.cpf,
+          cliente.nome,
+          cliente.telefone,
+          cliente.cep,
+          cliente.endereco,
+          cliente.cidade,
+          cliente.uf,
+          cliente.email,
+          cliente.senhaHash,
+        ],
+        (err) => {
+          if (err) {
+            reject({ erro: err });
+          } else {
+            resolve({
+              mensagem: `Cliente de cpf ${cpf} atualizado com sucesso.`,
+              cliente: cliente,
+              erro: false,
+            });
+          }
+        }
+      );
+    });
+  }
 }
-module.exports = ClientesDAO
+module.exports = ClientesDAO;
