35e57c7ec45f09cf5dd7b7c06022b48416f3180d
LuthGom <lucianomendes23@yahoo.com>
2022-07-04 23:13:00 -0300

    app.js: substituindo a instancia de database do sqlite3 pelo postgresql

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const routes = require('./routes');
-const { localStrategy, BearerStrategy } = require('./middlewares/estrategias-autenticacao');
-const bd = require('./infra/sqlite-db');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const routes = require("./routes");
+const {
+  localStrategy,
+  BearerStrategy,
+} = require("./middlewares/estrategias-autenticacao");
+const db = require("./infra/dbPG");
 localStrategy();
 BearerStrategy();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 app.use((req, res, next) => {
-    next()
-})
-routes(app)
+  next();
+});
+routes(app);
 
 module.exports = app;
