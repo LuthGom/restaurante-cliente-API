dd7ae609fe8428a0ce192bf61994df56fa19fc97
LuthGom <lucianomendes23@yahoo.com>
2022-07-19 17:54:47 -0300

    app.js: add middlewares do swagger para conectar devidamente com a doc gerada.

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const routes = require("./routes");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocs = require("./swagger.json");
 const {
   localStrategy,
   BearerStrategy,
@@ -11,6 +13,12 @@ localStrategy();
 BearerStrategy();
 
 app.use(express.json());
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.get("/terms", (req, res) => {
+  return res.json({
+    message: "Termos de Serviço",
+  });
+});
 app.use(cors());
 app.use((req, res, next) => {
   next();
